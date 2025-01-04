93c5cbc2c96922ef11f3db794facac3e5e1c8175
MaxiGitHub <maximojoaquin11@gmail.com>
2025-01-04 17:04:20 -0300

    bd sequelize

--- dds_backend/index.js
+++ dds_backend/index.js
@@ -2,8 +2,9 @@ const express = require("express");
 
 //crear servidor
 const app = express();
-app.use(express.json()); //para leer json en el body
 
+require("./base-orm/sqlite-init"); //crear base si no existe
+app.use(express.json()); //para leer json en el body
 app.use(express.json()); //para poder leer json en el body
 
 //controlar ruta 

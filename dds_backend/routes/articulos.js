1175bf07ff545fce8486c5981e205a132bbd36f3
MaxiGitHub <maximojoaquin11@gmail.com>
2025-01-09 20:00:27 -0300

    articulos.js terminado

--- dds_backend/routes/articulos.js
+++ dds_backend/routes/articulos.js
@@ -16,7 +16,7 @@ router.get("/api/articulos", async function (req, res, next){
     }
     const Pagina = req.query.Pagina ?? 1;
     const TamañoPagina = 10;
-    const { const, rows } = await db.articulos.findAndCountAll({
+    const { count, rows } = await db.articulos.findAndCountAll({
         atributes: [
             "IdArticulo",
             "Nombre",
@@ -67,7 +67,7 @@ router.post("/api/articulos/", async (req, res) => {
             Nombre: req.body.Nombre,
             Precio: req.body.Precio,
             CodigoDeBarra: req.body.CodigoDeBarra,
-            IdArticulosFamilia: req.body.IdArticulosFamilia,
+            IdArticuloFamilia: req.body.IdArticuloFamilia,
             Stock: req.body.Stock,
             FechaAlta: req.body.FechaAlta,
             Activo: req.body.Activo,
@@ -85,3 +85,99 @@ router.post("/api/articulos/", async (req, res) => {
         }
     }
 });
+
+router.put("/api/articulos/:id", async (req, res) => {
+    // #swagger.tags = ['Articulos']
+    // #swagger.summary = 'actualiza un Artículo'
+    // #swagger.parameters['id'] = { description: 'identificador del Artículo...' }
+    /*    #swagger.parameters['Articulo'] = {
+                    in: 'body',
+                    description: 'Articulo a actualizar',
+                    schema: { $ref: '#/definitions/Articulos' }
+        } */
+    try {
+        let item = await db.articulos.findOne({
+            attributes: [
+                "IdArticulo",
+                "Nombre",
+                "Precio",
+                "CodigoDeBarra",
+                "IdArticuloFamilia",
+                "Stock",
+                "FechaAlta",
+                "Activo",
+            ],
+            where: { IdArticulo: req.params.id },
+        });
+        if (!item) {
+            res.status(404).json({ message: "Articulo no encontrado" });
+            return;
+        }
+        item.Nombre = req.body.Nombre;
+        item.Precio = req.body.Precio;
+        item.CodigoDeBarra = req.body.CodigoDeBarra;
+        item.IdArticuloFamilia = req.body.IdArticuloFamilia;
+        item.Stock = req.body.Stock;
+        item.FechaAlta = req.body.FechaAlta;
+        item.Activo = req.body.Activo;
+        await item.save();
+        // otra forma de hacerlo
+        // let data = await db.articulos.update(
+        //   {
+        //     Nombre: req.body.Nombre,
+        //     Precio: req.body.Precio,
+        //     CodigoDeBarra: req.body.CodigoDeBarra,
+        //     IdArticuloFamilia: req.body.IdArticuloFamilia,
+        //     Stock: req.body.Stock,
+        //     FechaAlta: req.body.FechaAlta,
+        //     Activo: req.body.Activo,
+        //   },
+        //   { where: { IdArticulo: req.params.id } }
+        // );
+        res.sendStatus(204);
+    } catch (err) {
+        if (err instanceof ValidationError) {
+            let messages = '';
+            err.errors.forEach((x) => messages += x.path + ": " + x.message + '\n');
+            res.status(400).json({message : messages});
+        } else {
+            throw err;
+        }
+    }
+});
+
+router.delete("/api/articulos/:id", async (req, res) => {
+    // #swagger.tags = ['Articulos']
+    // #swagger.summary = 'elimina un Articulo'
+    // #swagger.parameters['id'] = { description: 'identificador del Articulo..' }
+    let bajaFisica = false;
+
+    if (bajaFisica) {
+        //baja fisica
+        let filasBorradas = await db.articulos.destroy({
+            where: { IdArticulo: req.params.id },
+        });
+        if (filasBorradas == 1) res.sendStatus(200);
+        else res-sendStatus(404);
+    } else {
+        //baja logica
+        try {
+            let data = await db.sequelize.query(
+                "UPDATE articulos SET Activo = case when Activo = 1 then 0 else 1 end WHERE IdArticulo = :IdArticulo",
+                {
+                    replacements: { IdArticulo: +req.params.id },
+                }
+            );
+            res.sendStatus(200);
+        } catch (err) {
+            if (err instanceof ValidationError) {
+                const messages = err.errors.map((x) => x.message);
+                res.status(400).json(messages);
+            } else {
+                throw err;
+            }
+        }
+    }
+});
+
+module.exports = router;
